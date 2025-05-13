ab0de6caa3f2cb7272f1fda857886fc493188a33
zakir Hossen <zakirhossen082@gmail.com>
2025-05-13 09:41:25 +0600

    New theme create & Theme Layout Structure (Header & Footer)

---
+++ wp-content/themes/wp-woocom/footer.php
@@ -0,0 +1,13 @@
+    <footer>
+        <section class="copyright">Copyright Area</section>
+        <section class="footer-menus">
+            <nav>
+                <a href="">Footer menu one</a>
+                <a href="">Footer menu two</a>
+                <a href="">Footer menu three</a>
+            </nav>
+        </section>
+     
+    </footer>
+</body>
+</html>
---
+++ wp-content/themes/wp-woocom/header.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>simple basic theme</title>
+</head>
+<body>
+    <header>
+        <section class="site-logo">Logo</section>
+        <section class="site-menus">
+                <nav>
+                    <a href="">Home</a>
+                    <a href="">About</a>
+                    <a href="">Products</a>
+                    <a href="">Contact</a>
+                </nav>
+        </section>
+    </header>
