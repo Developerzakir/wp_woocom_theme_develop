6db54eb9f13876bf853439730e2b84068129b01a
zakir Hossen <zakirhossen082@gmail.com>
2025-05-13 13:31:42 +0600

    Single Page Template & 404 Page create

---
+++ wp-content/themes/simple-bootstrap-theme/404.php
@@ -0,0 +1,10 @@
+<?php get_header(); ?>
+        <!-- Page content-->
+        <div class="container">
+            <div class="row">
+             <h1>File/Content not found</h1>
+            </div>
+        </div>
+     
+
+        <?php get_footer(); ?>
