5daf946ff52f7ee87e8dc63d3b5395dfa4b67f9c
zakir Hossen <zakirhossen082@gmail.com>
2025-05-13 10:00:53 +0600

    enqueue js file

--- wp-content/themes/wp-woocom/functions.php
+++ wp-content/themes/wp-woocom/functions.php
@@ -3,11 +3,14 @@
 
 function wpwoocom_theme_scripts_load()
 {
-    //theme custom theme.css
+    //theme custom css file theme.css
     wp_enqueue_style('theme-css', get_template_directory_uri()."/assets/css/theme.css", array(), '1.0', 'all');
 
-    //theme main style.css
+    //theme main file style.css
     wp_enqueue_style('theme-style', get_stylesheet_uri(), array(), '1.0', 'all');
+
+   //custom js file script.js
+    wp_enqueue_script('custom.js', get_template_directory_uri()."/assets/js/script.js", array('jquery'), '1.0', true);
 
 }
 add_action('wp_enqueue_scripts','wpwoocom_theme_scripts_load');
