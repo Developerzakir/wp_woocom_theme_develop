51757f1a08b12a554046cee813f558fd42fcbb86
zakir Hossen <zakirhossen082@gmail.com>
2025-05-13 09:46:32 +0600

    header file updated (language_attributes, bloginfo)

--- wp-content/themes/wp-woocom/header.php
+++ wp-content/themes/wp-woocom/header.php
@@ -1,9 +1,9 @@
 <!DOCTYPE html>
-<html lang="en">
+<html <?php language_attributes(); ?> >
 <head>
-    <meta charset="UTF-8">
+    <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>simple basic theme</title>
+    <title><?php bloginfo('title'); ?></title>
 </head>
 <body>
     <header>
