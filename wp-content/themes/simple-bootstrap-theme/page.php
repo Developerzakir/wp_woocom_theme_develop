e48f0e18602c6c27eee130bf04762db884bb2633
zakir Hossen <zakirhossen082@gmail.com>
2025-05-14 04:22:55 +0600

    Modifying WooCommerce Cart Page style

--- wp-content/themes/simple-bootstrap-theme/page.php
+++ wp-content/themes/simple-bootstrap-theme/page.php
@@ -1,48 +1,31 @@
 <?php get_header(); ?>
-        <!-- Page content-->
-        <div class="container">
-            <div class="row">
-                <!-- Blog entries-->
-              
-                  
 
-                    <?php
+<!-- Page Content -->
+<div class="container">
 
-                    if(have_posts()){
-                        while(have_posts()){
-                            the_post();
+    <div class="row">
 
-                            ?>
+        <?php
 
-                                <div class="card mb-4">
-                                    <a href="#!">
-                                       <?php if (has_post_thumbnail()) : ?>
-                                            <?php the_post_thumbnail('large', ['class' => 'card-img-top', 'alt' => get_the_title()]); ?>
-                                        <?php else : ?>
-                                            <img class="card-img-top" src="https://dummyimage.com/850x350/dee2e6/6c757d.jpg" alt="No image available">
-                                        <?php endif; ?>
-                                    </a>
+        if (have_posts()) {
 
-                                    <div class="card-body">
-                                        <div class="small text-muted"><?php echo get_the_date(); ?></div>
-                                        <h2 class="card-title"><?php the_title(); ?></h2>
-                                        <p class="card-text"><?php the_content(); ?></p>
-                                       
-                                    </div>
-                                </div>
+            while (have_posts()) {
 
-                            <?php 
-                        }
-                    }
-                    
-                    
-                    ?>
-                   
-               
-               
-             
-            </div>
-        </div>
-     
+                the_post();
+        ?>
+                <article class="col">
+                    <h2><?php the_title(); ?></h2>
+                    <p><?php the_content(); ?></p>
+                </article>
+        <?php
+            }
+        }
 
-        <?php get_footer(); ?>
+        ?>
+    </div>
+    <!-- /.row -->
+
+</div>
+<!-- /.container -->
+
+<?php get_footer(); ?>
